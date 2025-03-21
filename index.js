4870823285d13c17d5525d4aa60d91ed570fceef
NMget <megnet.noah@gmail.com>
2025-03-21 10:27:40 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const http = require("http");
+const swaggerJsDoc = require('swagger-jsdoc');
+const dotenv = require('dotenv');
 
+const swaggerGeneration = {
+    swaggerGeneration: {
+        openapi: "3.0.0",
+        info : {
+            title: 'anApi',
+            version: "0.1.0"
+        },
+        servers: [
+            {
+                url: "http://localhost:8081/api/v1",
+            },
+        ],
+    },
+    apis: ['src/routes/*.js']
+}
 const server = http.createServer (function(req, res) {
 // A l'arrivée d'une requête
 console.log("Serveur running at http://127.0.0.1:8081/");
-res.write("Hello World!");
 res.end();
 })
 
